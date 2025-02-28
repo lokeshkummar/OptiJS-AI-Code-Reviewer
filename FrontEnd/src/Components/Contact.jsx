e426bcd7316d70fa6c802f6dcfa7e0a86c70116d
Lokesh Kumar <lokeshkumar.080806@gmail.com>
2025-03-01 00:07:20 +0530

    change the contact component

--- FrontEnd/src/Components/Contact.jsx
+++ FrontEnd/src/Components/Contact.jsx
@@ -3,7 +3,7 @@
 const Contact = () => {
   return (
     <div className='h-screen w-full font-bold text-lg text-white flex flex-col justify-center items-center'>
-      <span>Email Me: <a href="mailto:lokeshkumar.080806@gmail.com" className="hover:text-yellow-300 hover:underline">Here</a></span>
+      <span>Email Me: <a href="mailto:lokeshkumar.080806@gmail.com" className="hover:text-yellow-300 hover:underline">Click Here</a></span>
     </div>
   )
 }
