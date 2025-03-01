ac637c14fc5d29bfc5169d118b7942d790eb47d9
Lokesh Kumar <lokeshkumar.080806@gmail.com>
2025-03-01 19:43:49 +0530

    fixing imports

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { ClerkProvider } from "@clerk/clerk-react";
 import "./index.css";
 import App from "./App.jsx";
