7390d6f4e5ffba8d01115c22c2691ff55c09a4fc
Lokesh Kumar <lokeshkumar.080806@gmail.com>
2025-03-01 19:39:42 +0530

    edit main component

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router";
-import { ClerkProvider } from "@clerk/clerk-react"
-import "./index.css"
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+import "./index.css";
 import App from "./App.jsx";
-import Hero from "./Components/Hero"
+import Hero from "./Components/Hero";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import TryNow from "./Components/TryNow";
@@ -10,34 +11,29 @@ import ProtectedRoute from "./Components/ProtectedRoute";
 import SignInPage from "./Components/SignInPage";
 import SignUpPage from "./Components/SignUpPage";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  throw new Error("Missing Publishable Key");
 }
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignedOutUrl="/" >
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<App />}>
           <Route index element={<Hero />} />
-          {/* <Route path="/try-now" element={<TryNow />} />
-          <Route path="/contact" element={<Contact />} /> */}
           <Route path="/about" element={<About />} />
         </Route>
 
         <Route path="/sign-in" element={<SignInPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
 
-        <Route element={<ProtectedRoute />}>
-          <Route path="/try-now" element={<TryNow />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
-
+        {/* Protecting specific routes */}
+        <Route path="/try-now" element={<ProtectedRoute><TryNow /></ProtectedRoute>} />
+        <Route path="/contact" element={<ProtectedRoute><Contact /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   </ClerkProvider>
